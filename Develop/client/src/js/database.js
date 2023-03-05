74c79136c288bb6285dfa9cfae67040e13c6f75e
ney2thehey <shetty.saineha@gmail.com>
2023-03-04 21:23:55 -0600

    updated comments and readme file

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,34 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
+  // Create a connection here to the database and version that we want to use, used from the mini proj
+    const jateDb = await openDB('jate', 1);
+        // Here we are creting a new transaction and this specifies the database and data privileges
+    const tx = jateDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+      const request = store.add({ id: 1, content: any });
+        // Getting confirmation of the request
+      const result = await request;
+    console.log('🚀 - data saved to the database', result);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error('getDb not implemented')
+
+    // Create a connection here to the database and version that we want to use, used from the mini proj
+  const jateDb = await openDB('jate', 1);
+    // Here we are creting a new transaction and this specifies the database and data privileges
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+ const request = store.get(1);
+
+  // Getting confirmation of the request
+ const result = await request;
+  console.log('🚀 - data saved to the database', result);
+  
+  ;}
 
 initdb();
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Text-Editor
+
+ ## Description
+ I will build a text editor that runs in the browser. The app will be a single-page application that meets the PWA criteria. Additionally, it will feature a number of data persistence techniques that serve as redundancy in case one of the options is not supported by the browser. The application will also function offline.
+
+
+  ## Table of Contents 
+  - [Installation](#installation)
+  - [Additional links](#additional-links)
+
+
+  ## Installation
+ Download and run in browser
+
+
+
+  ## Liscense 
+  ![Github License](https://img.shields.io/badge/license-MIT-blue.svg)
+
+  --
+  ## Additional Links
+  * Github: [ney2thehey](https://github.com/ney2thehey)
+  
